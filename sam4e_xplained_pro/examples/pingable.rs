cd06863ac9f964cb04e613b3207cda10f3bc59c9
John W. Terrell <john@coolpeoplenetworks.com>
2021-05-01 13:16:21 -0700

    WIP

--- sam4e_xplained_pro/examples/pingable.rs
+++ sam4e_xplained_pro/examples/pingable.rs
@@ -17,10 +17,10 @@ use sam4e_xplained_pro::{
 
 use smoltcp::wire::{
     IpAddress, IpCidr,
-    Ipv4Address, Icmpv4Repr, Icmpv4Packet
+    Ipv4Address, Icmpv4Repr, Icmpv4Packet,
 };
 use smoltcp::iface::{NeighborCache, EthernetInterfaceBuilder, Routes};
-
+use smoltcp::socket::{SocketSet, IcmpSocket, IcmpSocketBuffer, IcmpPacketMetadata, IcmpEndpoint};
 
 #[entry]
 fn main() -> ! {
@@ -83,20 +83,38 @@ fn main() -> ! {
         .routes(routes)
         .finalize();
 
+    // Create an ICMP socket
+    let mut rx_icmp_metadata_buffer:[IcmpPacketMetadata; 30] = [IcmpPacketMetadata::EMPTY; 30];
+    let mut tx_icmp_metadata_buffer:[IcmpPacketMetadata; 30] = [IcmpPacketMetadata::EMPTY; 30];
+
+    let mut rx_icmp_payload_buffer:[u8; 30] = [0; 30];
+    let mut tx_icmp_payload_buffer:[u8; 30] = [0; 30];
+
+    let icmp_rx_buffer = IcmpSocketBuffer::new(rx_icmp_metadata_buffer, rx_icmp_payload_buffer);
+    let icmp_tx_buffer = IcmpSocketBuffer::new(tx_icmp_metadata_buffer, tx_icmp_payload_buffer);
+    let icmp_socket = IcmpSocket::new(icmp_rx_buffer, icmp_tx_buffer);
+
     let mut link_detected = false;
     loop {
         let status = interface.device().status();
         if status.link_detected() != link_detected {
             if status.link_detected() {
-                hprintln!("Ethernet link is no UP with {} Mbps.", status.speed());
+                hprintln!("Ethernet link is no UP with {} Mbps.", status.speed()).unwrap();
             } else {
-                hprintln!("Ethernet link is now DOWN.");
+                hprintln!("Ethernet link is now DOWN.").unwrap();
             }
 
             link_detected = status.link_detected();
         }
 
         if status.link_detected() {
+//            let timestamp = Instant::now();
+            match interface.poll(&mut sockets, timestamp) {
+                Ok(_) => {},
+                Err(e) => {
+                    hprintln!("poll error: {}", e).unwrap();
+                }
+            }
         }
 
         // cortex_m::interrupt::free(|cs| {
